eed2870d00d76d6788910882d68f2f3ac537fa02
Prachi Gupta <prachiguptage@gmail.com>
2018-10-16 23:25:17 -0700

    adding changes

--- js/main.js
+++ js/main.js
@@ -161,11 +161,12 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = restaurant.name;
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
-  name.tabIndex = '3';
+  name.tabIndex = '2';
   li.append(name);
 
   const neighborhood = document.createElement('p');
@@ -179,7 +180,7 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  more.tabIndex = '3';
+  more.tabIndex = '2';
   li.append(more)
 
   return li
